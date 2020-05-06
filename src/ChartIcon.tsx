37a2739672509ebf78f7dbff98a61d019842c18c
Taylor Griffin <taylor.griffin.365@gmail.com>
2020-05-06 16:24:11 -0700

    Fixed homepage chart icon styling

--- src/ChartIcon.tsx
+++ src/ChartIcon.tsx
@@ -1,32 +1,39 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, useWindowDimensions } from "react-native";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 
 const ChartIcon = ({ title, goToChart }) => {
+  const windowWidth = useWindowDimensions().width;
+  console.info(windowWidth);
+  const windowHeight = useWindowDimensions().height;
+  const style = styles(windowWidth, windowHeight);
   return (
     <TouchableOpacity
-      style={styles.container}
+      style={style.container}
       onPress={() => {
         goToChart();
       }}
     >
-      <FontAwesome5Icon name="chart-line" style={styles.chartIcon} solid />
+      <FontAwesome5Icon name="chart-line" style={style.chartIcon} solid />
       <Text>{title}</Text>
     </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = (windowWidth : number, windowHeight : number) => StyleSheet.create({
   container: {
-    width: "20%",
-    height: "20%",
+    width: windowWidth * 0.216,
+    height: windowWidth * 0.216,
     borderWidth: 3,
     borderColor: "black",
     fontSize: 7,
-    padding: 10
+    padding: 10,
+    margin: 0.03 * windowWidth,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   chartIcon: {
-    fontSize: 20,
+    fontSize: 18,
     color: "black",
     textAlign: "center"
   }
